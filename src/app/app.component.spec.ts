b1b7cf680769e371c430767b71ef10ad84741183
mimranyousaf360 <mimranyousaf360@gmail.com>
2022-12-15 01:16:28 -0800

    Angular ProjectLocal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,4 +32,7 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('ValhallaAngular app is running!');
   });
+
+
+
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './ProjectComponents/header/header.component';
+import { FooterComponent } from './ProjectComponents/footer/footer.component';
+import { SliderComponent } from './ProjectComponents/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent,
   ],
   imports: [
     BrowserModule,
